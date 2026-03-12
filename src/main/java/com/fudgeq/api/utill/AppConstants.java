040b79f17f88ce82d595e290a39af4b037ee5b31
adheesha0316 <adheeshaanuradha2001@gmail.com>
2026-03-12 18:47:56 +0530

    cleaned AppConstants to follow Single Responsibility Principle

---
+++ src/main/java/com/fudgeq/api/utill/AppConstants.java
@@ -0,0 +1,7 @@
+package com.fudgeq.api.utill;
+
+public class AppConstants {
+    public static final String PREFIX_USER = "FQ-USR-";
+    public static final String PREFIX_PROFILE = "FQ-PRF-";
+    public static final String PREFIX_AUDIT = "FQ-AUD-";
+}
