9dd20aee4c6dd8222ef74fe939e40f7c2106fcf1
adheesha0316 <adheeshaanuradha2001@gmail.com>
2026-03-12 08:33:41 +0530

    Initial commit: Added Spring Boot starter files

---
+++ src/main/java/com/fudgeq/api/FudgeQECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.fudgeq.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FudgeQECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FudgeQECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fudgeq/api/FudgeQECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fudgeq.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FudgeQECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
