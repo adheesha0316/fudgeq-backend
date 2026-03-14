7e1cdc5a9172fa062d91909f031730ac7a5b7d2e
adheesha0316 <adheeshaanuradha2001@gmail.com>
2026-03-14 22:00:01 +0530

    enable asynchronous execution in Spring Boot

--- src/main/java/com/fudgeq/api/FudgeQECommerceApplication.java
+++ src/main/java/com/fudgeq/api/FudgeQECommerceApplication.java
@@ -2,8 +2,10 @@ package com.fudgeq.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class FudgeQECommerceApplication {
 
 	public static void main(String[] args) {
